2515802436856aab6a6061a5bb0d8fdf4f75972a
Pavle Lekic <pavle.lekic@htecgroup.com>
2018-02-25 18:52:04 +0100

    Added basic setup for express server, added session, npm dependencies etc.

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const routes = require('./routes');
+const config = require('./config.json');
+const cors = require('cors');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
+const secrets = require('./secrets.json');
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+app.use(session({
+    store: new FileStore(),
+    secret: secrets.session_secret,
+}));
+app.use(routes);
+
+app.listen(
+    config.port,
+    () => console.log(`Listening on port ${config.port}!`)
+);
